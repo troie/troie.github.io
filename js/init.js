f4bb97c279da1bd1ad29eb242668543a8105c2fa
Troie Pan <tpan@bitsxbites.com>
2016-04-08 14:02:20 +0800

    init new

---
+++ js/init.js
@@ -0,0 +1,13 @@
+(function ($) {
+    $(function () {
+        $('.slider').slider({
+            full_width: true,
+            height: 600
+        });
+       
+//        $('.slides').parallax();
+        $('.button-collapse').sideNav();
+
+    }); // end of document ready
+})(jQuery); // end of jQuery name space
+
