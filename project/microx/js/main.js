8538a5822db21b9f9e1e0c483d4b71f4d3413b03
Troie Pan <troiepan@gmail.com>
2017-04-21 15:47:48 +0800

    update

--- project/microx/js/main.js
+++ project/microx/js/main.js
@@ -1,10 +1,38 @@
 var video = document.getElementById('video');
 
 video.poster="poster.jpg";
-$(".content span").mouseenter(function(){
+$(".content img").mouseenter(function(){
     video.play();
+    
+    $(".content i").addClass("timeline")
+//    console.log("duration:"+video.duration)
+//    console.log("currentTime:"+video.currentTime)
+
+//    var i= video.currentTime;
+    var vidTime = video.duration;
+
+    var vidNow = video.currentTime;
+    var minStep = 30.8 / vidTime
+    var i=0;
+    console.log("i="+vidNow);
+//    console.log("vidTime="+vidTime+" is "+typeof(vidTime));
+//    console.log("minStep="+typeof(minStep));
+    console.log("vidTime="+vidTime);
+//    while(i <= vidTime){
+//        console.log("i="+i);
+//        $(".content i").css("width",i+"vh");
+//        i += minStep;
+//        if(i > vidTime) i=0;
+//    }
+//    for( i=vidNow.currentTime; i <=  4; i++){
+////        $(".content i").css("width",i+"vh");
+//        console.log("i="+i);
+////        i= i+.1;
+//    }
+    
 }).mouseleave(function(){
     video.pause();
+    $(".content i").removeClass("timeline")
 });
 
 window.onload=scaleVideo;
@@ -16,7 +44,7 @@ function scaleVideo(){
     //get window size
     var windowWidth = document.documentElement.clientWidth;
     var windowHeight = document.documentElement.clientHeight;
-    console.log(windowWidth +"......"+windowHeight)
+//    console.log(windowWidth +"......"+windowHeight)
     //get movie size
     var videoWidth = video.offsetWidth;
     var videoHeight = video.offsetHeight;
