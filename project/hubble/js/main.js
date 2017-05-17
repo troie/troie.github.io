15920582b73b3b3b15a0030731e63d98edd2fd0f
Troie Pan <troiepan@gmail.com>
2017-05-17 18:37:20 +0800

    update new hubble version

--- project/hubble/js/main.js
+++ project/hubble/js/main.js
@@ -1,27 +1,56 @@
 jQuery(document).ready(function ($) {
     $('.banner').unslider({
-        autoplay:true,
-        arrows:false
+        autoplay: true,
+        arrows: false
     });
-    
+    getMenuData();
     var loadFile = $(this).data('doc')
-    $(".canvas li").click(function(){
-        var url = "/"+$(this).data('doc')+".html";
-        location.href= (url); 
+    $(".menu").on("click", "li", function() {
+        getPage();
     });
-    
-    var url = "/js/main-page.json";
+
+    $(".page").on("click", "h2", function() {
+        $(".page").html("");
+//        $(".menu").css("height","100%");
+        getMenuData();
+//        alert(1);
+    });
+
+
+    //    var url = "/js/main-page.json";
+    //    $.getJSON(url, function (data) {
+    //        var items = [];
+    //        $.each(data, function (key, val) {
+    //            items.push("<li data-doc=\"html_" + val.doc_vol + "\"><aside><i>Vol." + val.doc_vol + "</i><strong>" + val.title_en + "</strong><p>" + val.title_cn + "</p></aside><img src=\"http://lorempixel.com/800/250/food/" + val.doc_vol + "\"></li>");
+    //
+    //        });
+    //
+    //        $("<ul/>", {
+    //            html: items.join("")
+    //        }).insertAfter("h1");
+    //
+    //    });
+
+    function getMenuData() {
+        var url = "/js/main-page.json";
         $.getJSON(url, function (data) {
             var items = [];
             $.each(data, function (key, val) {
-                items.push("<li data-doc=\"html_"+ val.doc_id +"\"><h1>"+val.title+"</h1><img src=\"http://lorempixel.com/800/200/food/"+ val.doc_id +"\"></li>");
-                
+                items.push("<li data-doc=\"html_" + val.doc_vol + "\"><aside><i>Vol." + val.doc_vol + "</i><strong>" + val.title_en + "</strong><p>" + val.title_cn + "</p></aside><img src=\"http://lorempixel.com/800/250/food/" + val.doc_vol + "\"></li>");
+
             });
-//            $
 
             $("<ul/>", {
                 html: items.join("")
-            }).insertAfter("h1");
+            }).appendTo(".menu");
 
         });
+    }
+
+    function getPage() {
+        $(".menu").html("");
+//        $(".menu").css("height",0);
+        var page = "<h2>Postproduction:Practically Magic</h2>";
+        $(page).appendTo(".page");
+    }
 });
