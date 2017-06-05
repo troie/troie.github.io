b8ac40729eacf0c725088db4f7b05d91b8cb22ca
Troie Pan <troiepan@gmail.com>
2017-06-05 10:32:42 +0800

    update griddle

--- project/griddle/js/main.js
+++ project/griddle/js/main.js
@@ -1,1 +1,88 @@
+$(document).ready(function () {
+    $('select').material_select();
+    $("#print-type").on("change", function () {
+        $(".sub-category").remove();
+        var url = "/js/" + $("#print-type").val() + ".json";
+        $.getJSON(url, function (data) {
+            var items = [];
+            $.each(data, function (key, val) {
+                items.push("<p><input type=\"checkbox\" id=\"comm_" + key + "\" value=\"" + key + "\"><label for=\"comm_" + key + "\">" + val.community + "</label></p>");
+            });
 
+            $("<div/>", {
+                "class": "sub-category",
+                "name": "sub-category",
+                html: items.join("")
+            }).insertAfter(".input-field");
+
+        });
+    });
+    
+    $("div").on("click", ".sub-category input", function () {
+        event.stopPropagation();
+        var commnuity_key = $(this).val();
+        var commnuity = "comm-" + commnuity_key;
+        if ($(this).prop("checked")) {
+            var url = "/js/" + $("#print-type").val() + ".json";
+            $.getJSON(url, function (data) {
+                var team_member = [];
+                team_member.push("<h3>" + data[commnuity_key].community + "</h3>");
+                $.each(data[commnuity_key].member, function (key, val) {
+                    team_member.push("<li><input type=\"checkbox\" id=\"" + commnuity + "-" + key + "\" name=\"mbr_id\" checked /><label for=\"" + commnuity + "-" + key + "\">" + data[commnuity_key].member[key].nameEn + "</label></li>");
+                });
+
+                $("<ul/>", {
+                    "class": commnuity + " l_grid clearfix",
+                    html: team_member.join("")
+                }).appendTo(".main");
+            });
+        } else {
+
+            $("." + commnuity).remove();
+        }
+
+    });
+
+    $(".add").on("click", function () {
+        event.stopPropagation();
+        var preview_arr = [];
+        $(".main li input").each(function (i) {
+            if ($(this).prop("checked")) {
+                preview_arr.push($(this).attr("id"));
+            }
+        });
+        var tmp = [];
+        var tmp_comm_key;
+        var url = "/js/" + $("#print-type").val() + ".json";
+        $.getJSON(url, function (data) {
+            var print_arr = [];
+
+//            team_member.push("<h3>" + data[commnuity_key].community + "</h3>");
+            $.each(preview_arr,function(index){
+//                console.log(preview_arr[index])
+                tmp = preview_arr[index].split("-");
+                if( tmp_comm_key == "undefine") tmp_comm_key = tmp[1];
+                if( tmp[1] != tmp_comm_key){
+                    print_arr.push("<div class=\"comm_title\"><h4>"+data[tmp[1]].community+"</h4></div>");
+                    tmp_comm_key = tmp[1];
+                }
+
+                var card_data = title = "";
+                card_data = "<div><h4>"+data[tmp[1]].member[tmp[2]].nameEn+"</h4>";
+                card_data += "<strong>"+data[tmp[1]].member[tmp[2]].nameCh+"</strong>";
+                card_data += "<span>"+data[tmp[1]].member[tmp[2]].title.join("")+"</span></div>";
+                print_arr.push(card_data);
+            });
+            $(print_arr.join("")).appendTo(".preview");
+//            console.log(print_arr)
+            sessionStorage.print_arr = print_arr;
+            sessionStorage.preview_arr = preview_arr;
+            $(".preview").css("display","block");
+            $.print(".preview" /*, options*/);
+        });
+        
+        
+
+    });
+
+});
--- project/hubble/js/main.js
+++ project/hubble/js/main.js
@@ -1,8 +1,4 @@
 jQuery(document).ready(function ($) {
-    //    $('.banner').unslider({
-    //        autoplay: true,
-    //        arrows: false
-    //    });
 
     getData();
 
@@ -18,8 +14,7 @@ jQuery(document).ready(function ($) {
     });
 
     $(".page").on("click", "li", function () {
-        //        alert($(this).data('id'));
-        getComment();
+        getComment($(this).data('index'));
 
     });
     $(".comment").on("click", "div", function () {
@@ -32,11 +27,13 @@ jQuery(document).ready(function ($) {
     });
 
     function getData() {
+        localStorage.removeItem('pageId');
+        localStorage.removeItem('title');
         var url = "/js/main-page.json";
         $.getJSON(url, function (data) {
             var items = [];
             $.each(data, function (key, val) {
-                items.push("<li data-index=\"" + key + "\"><aside><i>Vol." + val.doc_vol + "</i><strong>" + val.title_en + "</strong><p>" + val.title_cn + "</p></aside><img src=\"http://lorempixel.com/800/250/food/" + val.doc_vol + "\"></li>");
+                items.push("<li data-index=\"" + key + "\"><aside><i>Vol." + val.doc_vol + "</i><strong>" + val.title_en + "</strong><p>" + val.title_cn + "</p></aside><img src=\"http://lorempixel.com/800/500/food/" + key + "\"></li>");
             });
 
             $("<ul/>", {
@@ -44,25 +41,17 @@ jQuery(document).ready(function ($) {
             }).appendTo(".menu");
 
         });
+//        console.log("pageId"+localStorage.getItem('pageId'));
     }
 
     function getPage(pageId) {
         $(".menu").html("");
-        //        if(localStorage.getItem("pageId")) pageId = localStorage.getItem("pageId");
-        //        localStorage.setItem("pageId", pageId);
+        if(!localStorage.getItem('pageId')) localStorage.setItem('pageId', pageId);
+        else pageId = localStorage.getItem('pageId');
 
         var url = "/js/main-page.json";
         $.getJSON(url, function (data) {
-            //            var items = [];
-            //            $.each(data, function (key, val) {
-            //                items.push("<li data-doc=\"html_" + val.doc_vol + "\"><aside><i>Vol." + key+"."+val.doc_vol + "</i><strong>" + val.title_en + "</strong><p>" + val.title_cn + "</p></aside><img src=\"http://lorempixel.com/800/250/food/" + val.doc_vol + "\"></li>");
-            //            });
-            //
-            //            $("<ul/>", {
-            //                html: items.join("")
-            //            }).appendTo(".menu");
-            //            alert(data[pageId].description)
-
+            localStorage.setItem('title', data[pageId].title_en);
             var page = "<div class=\"page_up\">" +
                 "<h1>" + data[pageId].title_en + "</h1>" +
                 "<ascid><i>" + data[pageId].score + "</i>豆瓣评分</ascid>" +
@@ -73,61 +62,34 @@ jQuery(document).ready(function ($) {
                 "<dt>Runtime</dt><dd>" + data[pageId].runtime + "</dd>" +
                 "<dt>Year</dt><dd>" + data[pageId].year + "</dd>" +
                 "</dl>" +
-                "<img src='img/doc_" + data[pageId].doc_vol + ".jpg'>" +
+                "<img src='http://lorempixel.com/270/360/food/" + pageId + "'>" +
+//                "<img src='img/doc_" + data[pageId].doc_vol + ".jpg'>" +
                 "<p>" + data[pageId].description + "</p>" +
                 "</div>";
             $(page).appendTo(".page");
             getSliderData(pageId);
-            //                console.log(slider)
-            //                page += "<div class=\"page_down\">" +slider+
-            //                "<div class=\"slider\">" +
-            //                "<ul>" +
-            //                "<li data-id=\"1\">" +
-            //                "<b>Harry</b>" +
-            //                "<p>超爱 Peaseful Cusine 系列视频，原因是制作人从新的角度展示食物，我们常常从视觉嗅觉味觉来观察食物，唯独忘了听觉，不加配乐，只用切菜、开火、煮汤等做菜同期声，在极简影像风格中放大听觉，刺激观众的想象。</p>" +
-            //                "</li>" +
-            //                "<li data-id=\"2\">" +
-            //                "<b>Tim</b>" +
-            //                "<p>超爱 Peaseful Cusine 系列视频，原因是制作人从新的角度展示食物，我们常常从视觉嗅觉味觉来观察食物，唯独忘了听觉，不加配乐，只用切菜、开火、煮汤等做菜同期声，在极简影像风格中放大听觉，刺激观众的想象。</p>" +
-            //                "</li>" +
-            //                "<li data-id=\"3\">" +
-            //                "<b>Troie</b>" +
-            //                "<p>超爱 Peaseful Cusine 系列视频，原因是制作人从新的角度展示食物，我们常常从视觉嗅觉味觉来观察食物，唯独忘了听觉，不加配乐，只用切菜、开火、煮汤等做菜同期声，在极简影像风格中放大听觉，刺激观众的想象。</p>" +
-            //                "</li>" +
-            //                "</ul>" +
-            //                "</div>" +
-            //                "</div>" +
-            //                "<img class=\"background\" src=\"img/doc_1.jpg\">";
-            $("<img class=\"background\" src=\"img/doc_1.jpg\">").appendTo(".page");
+            $("<img class=\"background\" src=\"http://lorempixel.com/270/360/food/"+pageId+"\">").appendTo(".page");
+//            $("<img class=\"background\" src=\"img/doc_1.jpg\">").appendTo(".page");
         });
-
-
-
-
+//        console.log("pageId"+localStorage.getItem('pageId'));
     }
 
     function getSliderData(pageId) {
         var url = "/js/comment/comment_" + pageId + ".json";
-        //        alert(url)
         var sliderData;
         $.getJSON(url, function (data) {
-            //            var items = [];
-            //            $.each(data, function (key, val) {
-            //                items.push("<li data-index=\"" + key + "\"><b>"+val.commentator+"</b><p>"+val.comment+"</P</li>");
-            //            });
-            //            
-            //            slider = "<div class=\"slider\"><ul>"+items.join("")+"</ul></div>";
-            //            page = "<div class=\"page_down\">" +slider+"</div>";
             sliderData = data;
             makeSlider(data)
         });
     }
 
     function makeSlider(sliderData) {
-        var slider, page;
+        var slider, page, comment;
         var items = [];
         $.each(sliderData, function (key, val) {
-            items.push("<li data-index=\"" + key + "\"><b>" + val.commentator + "</b><p>" + val.comment + "</P</li>");
+            comment = val.comment.split("\n");
+//            console.log(comment)
+            items.push("<li data-index=\"" + key + "\"><b>" + val.commentator + "</b><p>" + comment[0] + "</P</li>");
         });
         slider = "<div class=\"slider\"><ul>" + items.join("") + "</ul></div>";
         page = "<div class=\"page_down\">" + slider + "</div>";
@@ -140,28 +102,35 @@ jQuery(document).ready(function ($) {
         });
     }
 
-    function getComment() {
+    function getComment(commId) {
         $(".page").html("");
-        //        alert(1)
-        var content = "<header>" +
-            "<h1>Postproduction:Practically Magic</h1>" +
+        
+        var pageId = localStorage.getItem('pageId');
+        var url = "/js/comment/comment_" + pageId + ".json";
+        var sliderData;
+        $.getJSON(url, function (data) {
+            var content = "<header>" +
+            "<h1>"+commId+" "+localStorage.getItem('title')+"</h1>" +
             "</header>" +
             "<div>" +
-            "<b>Harry</b>" +
-            "<p>故事虽老套但表演非常细腻。小女孩和美队太有爱，尤其爱夕阳日落在海滩的那段，小姑娘在美队身上爬来爬去的太甜。美队说要少接甚至不接超级英雄角色，因为有碍于他获得别的角色或者投入精力去演好别的角色，真是太对了。美队还是很有潜力去演一些更深刻更细腻的角色的，从午夜邂逅开始就这么认为。</p>" +
-            "<p>“He wanted me before I was smart”。简单却惊喜的一部片，不止故事好，包括小萝莉在内的几位演员都表现抢眼。现实中家长都希望自己孩子成为天才，而美队饰演的舅舅却希望自己的天才外甥女能像普通人一样生活，家长们真应该组团来看这部片啊。另外，美队哪像当过哲学教授的人啊，体育学教授更像吧。</p>" +
-            "<p>其实看这部电影除了获得满满的温馨之外，更让我受益的是男主的与侄女交流的方式， 印象较深的是两个场景： 1.男主因为Mary看到自己的老师早晨出现他们家里而感到尴尬，就对Mary发了火，说了一些气话，然后Mary就自己窝在床上生闷气，男主过了一段时间来向Mary道歉，请求原谅</p>" +
-            "<p>其实看这部电影除了获得满满的温馨之外，更让我受益的是男主的与侄女交流的方式， 印象较深的是两个场景： 1.男主因为Mary看到自己的老师早晨出现他们家里而感到尴尬，就对Mary发了火，说了一些气话，然后Mary就自己窝在床上生闷气，男主过了一段时间来向Mary道歉，请求原谅</p>" +
-            "</div>" +
+            "<b>"+data[commId].commentator+"</b>" + makeComment(data[commId].comment) + "</div>" +
             "<footer></footer>" +
-            "<img class=\"background\" src=\"img/doc_1.jpg\">";
-        //        console.log(content2);
-        //        alert(content)
-        $(content).appendTo(".comment");
+            "<img class=\"background\" src=\"http://lorempixel.com/270/360/food/"+pageId+"\">";
+
+            $(content).appendTo(".comment");
+        });
     }
 
-    function getArea() {
+    function makeComment( str ) {
         //        var inputValue = document.getElementById("txtArea").value;
-        var content = inputValue.replace(/\r\n/g, "\n").split("\n");
+       
+        var content = (str).split("\n");
+         var items=[];
+        $.each(content, function (key, val) {
+            
+//            console.log(comment)
+            items.push("<p>" + val + "</p>");
+        });
+        return items.join("");
     }
 });
