bfcef3a3a12b2a97b2bc269546a582a13b3f0c47
Troie Pan <troiepan@gmail.com>
2017-06-19 16:46:27 +0800

    mobile

--- project/ax/js/main.js
+++ project/ax/js/main.js
@@ -1,5 +1,22 @@
 //var userAgent = window.navigator.userAgent.toLowerCase();
-//alert(userAgent)
+////alert(userAgent + "/r/n"+ $.browser.version)
+//alert( $.browser.version );
+
+var sBrowser, sUsrAg = navigator.userAgent;
+
+if(sUsrAg.indexOf("Chrome") > -1) {
+    sBrowser = "Google Chrome";
+} else if (sUsrAg.indexOf("Safari") > -1) {
+    sBrowser = "Apple Safari";
+} else if (sUsrAg.indexOf("Opera") > -1) {
+    sBrowser = "Opera";
+} else if (sUsrAg.indexOf("Firefox") > -1) {
+    sBrowser = "Mozilla Firefox";
+} else if (sUsrAg.indexOf("MSIE") > -1) {
+    sBrowser = "Microsoft Internet Explorer";
+}
+
+alert("You are using: " + sBrowser);
 
 window.onload = scaleVideo;
 window.onresize = scaleVideo;
